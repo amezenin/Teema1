81a0f7b1d5a29ff9230a36408a368c42c6ea9ccb
Anton <anton.mezhenin@gmail.com>
2016-10-16 20:13:09 +0300

    Teema 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Osa 1");
+        int a = 10;
+        while (a > 0) {
+
+            System.out.println(a);
+            a--;
+
+        }
+        System.out.println("Osa 2");
+
+        int b = 0;
+        while (b < 10) {
+            b++;
+            if (b % 2 == 0){
+                System.out.println(b);
+            }
+
+        }
+
+        System.out.println("Osa 3");
+        int tabel [][] = new int [10][10];
+        int count1 = 0;
+        int count2 = 0;
+        while (count1 < 10) {
+            count2 = 0;
+            while (count2 < 10) {
+                for (int i = 0; i < 10; i++) {
+                    tabel [count1][count2] = count1 + count2;
+                    count2++;
+                }
+
+            }
+            count1++;
+
+        }
+        System.out.print(Arrays.deepToString(tabel));
+
+        // write your code here
+    }
+
+}
